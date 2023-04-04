c1482e00b838e7559ec0c60e7edbddb79cf268a5
Bianca Aspin <bianca@mj94.net>
2023-04-04 13:52:54 -0700

    Initialize Flask-Migrate.

---
+++ server/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, request, session, make_response, jsonify, abort, render_template
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+from werkzeug.exceptions import NotFound, Unauthorized
+from config import app, db, api
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
