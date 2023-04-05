7bb0a5f38ae508e04ab6e5b94eeb3f5ae7e1e352
Bianca Aspin <bianca@mj94.net>
2023-04-05 16:31:33 -0700

    Build out login form which saves user's id to a session and redirects to main page if successful, renders errors if not.

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,5 +1,111 @@
+import { 
+    Heading,
+    Box,
+    Stack,
+    Input,
+    InputGroup,
+    InputRightElement, 
+    Button,
+    FormControl,
+    FormLabel,
+    FormErrorMessage 
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { useFormik } from 'formik';
+import { useHistory } from 'react-router-dom'
+import * as yup from 'yup';
+
 function Login() {
-    return <h1>Hello from the Login</h1>
+
+    const [showPassword, setShowPassword] = useState(false)
+    const [error, setError] = useState(null)
+    
+    function handlePasswordToggle() {
+        setShowPassword(!showPassword)
+    }
+
+    const history = useHistory()
+    
+    const formSchema = yup.object().shape({
+        email: yup
+            .string()
+            .email("Must enter valid email address.")
+            .required("Must enter email."),
+        password: yup
+            .string()
+            .required("Must enter password."),
+    })
+    
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: formSchema,
+        validateOnChange: false,
+        validateOnBlur: false,
+        onSubmit: (values) => {
+            console.log(values)
+            fetch('/login', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(values),
+            })
+            .then(response => {
+                if (response.ok) {
+                    response.json()
+                    .then(userData => console.log(userData))
+                    .then(history.push('/'))
+                } else {
+                    response.json()
+                    .then(error => setError(error['error']))
+                }
+            })
+        }
+    });
+    
+    return (
+        
+        <Box w="400px" margin="auto">
+
+            <Stack>
+
+                <Heading>Log In to Your Account</Heading>
+
+                {error 
+                    ? <Box bg="red">Error: {error}</Box>
+                    : null
+                }
+
+                <form onSubmit={formik.handleSubmit}>
+
+                    <FormControl isInvalid={formik.errors['email']}>
+                        <FormLabel>Email Address</FormLabel>
+                        <Input type="text" name="email" value={formik.values.email} onChange={formik.handleChange}/>
+                        <FormErrorMessage>{formik.errors['email']}</FormErrorMessage>
+                    </FormControl>
+
+                    <FormControl isInvalid={formik.errors['password']}>
+                        <FormLabel>Password</FormLabel>
+                        <InputGroup>
+                            <Input type={showPassword ? "text" : "password"} name="password" value={formik.values.password} onChange={formik.handleChange}/>
+                            <InputRightElement>
+                                <Button onClick={handlePasswordToggle}>{showPassword ? 'Hide' : 'Show'}</Button>
+                            </InputRightElement>
+                        </InputGroup>
+                        <FormErrorMessage>{formik.errors['password']}</FormErrorMessage>
+                    </FormControl>
+
+                    <Button type="submit">Log In</Button>
+
+                </form>
+
+            </Stack>
+
+        </Box>
+    )
 }
 
 export default Login
