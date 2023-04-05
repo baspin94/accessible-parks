c7dccbccc781f8e948d4bdc86c85627298da265b
Bianca Aspin <bianca@mj94.net>
2023-04-04 20:55:07 -0700

    Create user model with password hashing.

--- server/models.py
+++ server/models.py
@@ -8,12 +8,28 @@ class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key = True)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-    email = db.Column(db.String)
-    _password_hash = db.Column(db.String)
+    first_name = db.Column(db.String, nullable = False)
+    last_name = db.Column(db.String, nullable = False)
+    email = db.Column(db.String, unique = True)
+    _password_hash = db.Column(db.String, nullable = False)
     created_at = db.Column(db.DateTime, server_default = db.func.now())
     updated_at = db.Column(db.DateTime, onupdate = db.func.now())
 
+    @hybrid_property
+    def password_hash(self):
+        raise Exception("Password hashes cannot be viewed.")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, 
+            password.encode('utf-8'))
+    
     def __repr__(self):
         return f'<User: {self.first_name} {self.last_name}'
