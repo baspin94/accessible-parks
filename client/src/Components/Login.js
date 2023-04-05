72e60c3dcf95dc3a97dd5e39359ee2c69f6075af
Bianca Aspin <bianca@mj94.net>
2023-04-05 10:30:48 -0700

    Create initial components with client-side routing.

---
+++ client/src/Components/Login.js
@@ -0,0 +1,5 @@
+function Login() {
+    return <h1>Hello from the Login</h1>
+}
+
+export default Login
---
+++ client/src/Components/Signup.js
@@ -0,0 +1,5 @@
+function Signup() {
+    return <h1>Salutations from Signup</h1>
+}
+
+export default Signup;
