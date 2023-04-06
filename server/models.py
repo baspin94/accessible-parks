0accfb597cecddc0bc75a2c2f0431ef9e84724c3
Bianca Aspin <bianca@mj94.net>
2023-04-06 12:10:07 -0700

    Add model and migration for 'parks' table.

--- server/models.py
+++ server/models.py
@@ -48,3 +48,31 @@ class Amenity(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f'<Amenity: {self.name}>'
+    
+class Park(db.Model, SerializerMixin):
+    __tablename__ = "parks"
+
+    serialize_rules = ('-created_at', '-updated_at')
+
+    id = db.Column(db.Integer, primary_key = True)
+    nps_api_id = db.Column(db.String)
+    name = db.Column(db.String)
+    code = db.Column(db.String)
+    states = db.Column(db.String)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    designation = db.Column(db.String)
+    description = db.Column(db.String)
+    phone = db.Column(db.String)
+    email = db.Column(db.String)
+    image_url = db.Column(db.String)
+    image_alt = db.Column(db.String)
+    image_credit = db.Column(db.String)
+    weather = db.Column(db.String)
+    nps_url = db.Column(db.String)
+    access_url = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    def __repr__(self):
+        return f'<Park: {self.name} ({self.code})>'
