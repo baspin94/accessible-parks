c7fcdb09fb7d0749c5821258f7f91aeecfb9c121
Bianca Aspin <bianca@mj94.net>
2023-04-06 10:30:50 -0700

    Configure import of environment variables and create initial script for retrieving data from NPS API.

--- server/config.py
+++ server/config.py
@@ -8,13 +8,18 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
 
 # Local imports
+import os
 
 
 # Instantiate app, set attributes
+
+load_dotenv()
+
 app = Flask(__name__)
-app.secret_key = b'\xb1|\xb4\xc1\xb2\x9d[d$\x16CC\xce\x82\xfbm'
+app.secret_key = os.environ.get('APP_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
