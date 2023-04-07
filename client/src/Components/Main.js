09afd264e66605361ac373a9cfef47e118d3951e
Bianca Aspin <bianca@mj94.net>
2023-04-07 12:02:27 -0700

    Fetch and render amenities on page as checkboxes.

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -1,11 +1,53 @@
+import {
+    Checkbox,
+    Heading,
+    Grid
+} from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+
 function Main({user}) {
+    const [amenities, setAmenities] = useState([])
+
+    useEffect( () => {
+        fetch('/amenities')
+            .then(response => response.json())
+            .then(amenityData => setAmenities(amenityData))
+    }, [])
+
+    const checkboxes = amenities.map( (amenity) => {
+        return (
+            <Checkbox
+                // justifyContent='center' 
+                p='5px'
+                size='md' 
+                border='1px'
+                key={amenity.id}
+            >
+                {amenity.name}
+            </Checkbox>
+        )
+    })
 
     return (
-        <h2>{
-            (user.first_name !== undefined)
-                ? `Welcome, ${user.first_name}!`
-                :  'Welcome!'
-            }</h2>
+        <>
+            <Heading p="20px">
+                {
+                (user.first_name !== undefined)
+                    ? `Welcome, ${user.first_name}!`
+                    : 'Welcome!'
+                }
+            </Heading>
+            <Grid margin="auto" w="50%" templateColumns='repeat(3, 1fr)' gap={2} justifyContent="center">
+                {checkboxes}
+            </Grid>
+            
+            
+        
+        
+        
+        
+        </>
+        
     )
 }
 
