633af10b91b1098f56bd1f3b5f06e2f704b10d59
Bianca Aspin <bianca@mj94.net>
2023-04-07 15:01:13 -0700

    Search form now adds ids of checked amenities to an array which is logged to the console upon submit.

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -1,9 +1,11 @@
 import {
     Checkbox,
     Heading,
-    Grid
+    Grid,
+    Button, 
 } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
+import { useFormik } from 'formik';
 
 function Main({user}) {
     const [amenities, setAmenities] = useState([])
@@ -11,17 +13,30 @@ function Main({user}) {
     useEffect( () => {
         fetch('/amenities')
             .then(response => response.json())
-            .then(amenityData => setAmenities(amenityData))
+            .then(amenityData => setAmenities(amenityData.sort((a1, a2) => (a1.name > a2.name) ? 1 : -1)))
     }, [])
+
+    const formik = useFormik({
+        initialValues: {
+            checked: []
+        },
+        onSubmit: (values) => {
+            const int_values = values['checked'].map((value) => parseInt(value))
+            console.log(int_values)
+        }
+    })
+
 
     const checkboxes = amenities.map( (amenity) => {
         return (
             <Checkbox
-                // justifyContent='center' 
                 p='5px'
                 size='md' 
                 border='1px'
                 key={amenity.id}
+                name="checked"
+                onChange={formik.handleChange}
+                value={amenity.id}
             >
                 {amenity.name}
             </Checkbox>
@@ -37,9 +52,13 @@ function Main({user}) {
                     : 'Welcome!'
                 }
             </Heading>
-            <Grid margin="auto" w="50%" templateColumns='repeat(3, 1fr)' gap={2} justifyContent="center">
-                {checkboxes}
-            </Grid>
+            <form onSubmit={formik.handleSubmit}>
+                <Grid margin="auto" w="50%" templateColumns='repeat(3, 1fr)' gap={2} justifyContent="center">
+                    {checkboxes}
+                </Grid>
+                <Button type="submit">Search</Button>
+            </form>
+            
             
             
         
