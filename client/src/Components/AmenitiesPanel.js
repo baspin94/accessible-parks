9d5593dfe95213abdbf6d62ee5b582e6b22f6d1a
Bianca Aspin <bianca@mj94.net>
2023-04-09 14:02:01 -0700

    Initial placement of elements.

--- client/src/Components/AmenitiesPanel.js
+++ client/src/Components/AmenitiesPanel.js
@@ -1,5 +1,25 @@
-function AmenitiesPanel( {park }) {
-    return <h1>Amenities Panel for {park.name}</h1>
+import {
+    Box, 
+    Heading, 
+    Text,
+    UnorderedList,
+    ListItem
+} from '@chakra-ui/react';
+
+function AmenitiesPanel({ park }) {
+
+    const amenities = park.park_amenities.map(element => {
+        return <ListItem key={element.amenity.id}>{element.amenity.name}</ListItem>
+    })
+
+    return (
+        <Box>
+            <Heading as="h3">Accessibility-Related Amenities</Heading>
+            <UnorderedList>
+                {amenities}
+            </UnorderedList>
+        </Box>
+    )
 }
 
 export default AmenitiesPanel;
