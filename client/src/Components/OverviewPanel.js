86148744c6f76ff6a506eb8953a7f18aa3eea176
Bianca Aspin <bianca@mj94.net>
2023-04-09 13:22:33 -0700

    Initial placement of elements.

--- client/src/Components/OverviewPanel.js
+++ client/src/Components/OverviewPanel.js
@@ -1,5 +1,24 @@
+import {
+    Box, 
+    Heading, 
+    Text, 
+    Image
+} from '@chakra-ui/react';
+
 function OverviewPanel( { park }) {
-    return <h1>Overview Panel for {park.name}</h1>
+    return (
+        <>
+        <Box w="60%" border='1px'>
+            <Heading>{park.name}</Heading>
+            <Text>{park.designation} | {park.states}</Text>
+            <Box boxSize="md" border='1px'>
+                <Image src={park.image_url} alt={park.image_alt}/>
+            </Box>
+            <Text>{park.description}</Text>
+        </Box>
+        </>
+    )
+    
 }
 
 export default OverviewPanel;
