74e65c920acb1a81039ec5667448d1ce421fd4b4
Bianca Aspin <bianca@mj94.net>
2023-04-09 13:50:59 -0700

    Initial placement of DetailsPanel elements, installation of icon packages.

--- client/src/Components/DetailsPanel.js
+++ client/src/Components/DetailsPanel.js
@@ -1,5 +1,50 @@
+import {
+    Box, 
+    Heading, 
+    Text,
+    UnorderedList,
+    ListItem, 
+    ListIcon,
+} from '@chakra-ui/react';
+import {
+    PhoneIcon,
+    EmailIcon,
+    InfoOutlineIcon,
+    CheckIcon
+} from '@chakra-ui/icons'
+
 function DetailsPanel({ park }) {
-    return <h1>Details Panel for {park.name}</h1>
+
+    return (
+        <Box>
+            <Heading as="h3">General Information</Heading>
+            <Heading as="h4">National Park Service Pages</Heading>
+                <UnorderedList styleType="none">
+                    <ListItem>
+                        <ListIcon as={InfoOutlineIcon}/>
+                        General:
+                    </ListItem>
+                    <ListItem>
+                        <ListIcon as={CheckIcon}/>
+                        Accessibility:
+                    </ListItem>
+
+                </UnorderedList>
+            <Heading as="h4">Contact Information</Heading>
+                <UnorderedList styleType="none">
+                    <ListItem>
+                        <ListIcon as={PhoneIcon}/>
+                        Phone: {park.phone}
+                    </ListItem>
+                    <ListItem>
+                        <ListIcon as={EmailIcon}/>
+                        Email: {park.email}
+                    </ListItem>
+                </UnorderedList>
+            <Heading as="h4">Weather</Heading>
+                <Text>{park.weather}</Text>
+        </Box>
+    )
 }
 
 export default DetailsPanel;
