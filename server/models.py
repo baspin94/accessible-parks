bfcc5df8e42175b899f5a7f331146df9cc38a52b
Bianca Aspin <bianca@mj94.net>
2023-04-09 19:20:58 -0700

    Update serialize rules for 'user_parks' table.

--- server/models.py
+++ server/models.py
@@ -100,6 +100,8 @@ class ParkAmenity(db.Model, SerializerMixin):
 class UserPark(db.Model, SerializerMixin):
     __tablename__ = "user_parks"
 
+    serialize_only = ('id', 'park_id', 'user_id', 'created_at')
+
     id = db.Column(db.Integer, primary_key = True)
     park_id = db.Column(db.Integer, db.ForeignKey('parks.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
