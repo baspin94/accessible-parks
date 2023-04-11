ee656c613ad212378a4f0deab9d470dd99808c2c
Bianca Aspin <bianca@mj94.net>
2023-04-11 10:49:08 -0700

    Create initial review card and render in Review Panel.

---
+++ client/src/Components/ReviewCard.js
@@ -0,0 +1,5 @@
+function ReviewCard() {
+    return <h3>Review Card</h3>
+}
+
+export default ReviewCard;
--- client/src/Components/ReviewPanel.js
+++ client/src/Components/ReviewPanel.js
@@ -4,13 +4,19 @@ import {
     Grid,
     UnorderedList,
 } from '@chakra-ui/react';
+import ReviewCard from './ReviewCard';
 
 function ReviewPanel({ park }) {
+
+    const reviews = park.reviews.map(element => {
+        return <ReviewCard key={element.id}/>
+    })
+
     return (
         <Box p="10px">
             <Heading as="h3" size="lg">Reviews</Heading>
             <UnorderedList>
-                
+                {reviews}
             </UnorderedList>
         </Box>
     )
