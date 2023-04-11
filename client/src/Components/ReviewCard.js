1f453a45cd837d451b5efd9cf720b9e8d5185ab1
Bianca Aspin <bianca@mj94.net>
2023-04-11 12:25:50 -0700

    User can now toggle edit mode on their own reviews and submit a review PATCH request. Update currently does not render on DOM until refresh.

--- client/src/Components/ReviewCard.js
+++ client/src/Components/ReviewCard.js
@@ -1,17 +1,111 @@
 import { UserContext } from '../App';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import {
+    Card,
+    CardHeader,
+    Heading,
+    Text,
+    CardBody,
+    CardFooter,
+    Button,
+    Icon,
+    FormLabel,
+    Input,
+    Textarea,
+    Select
+} from '@chakra-ui/react';
+
+import { useFormik } from 'formik';
+
 
 function ReviewCard({ review }) {
+    const [editMode, setEditMode] = useState(false)
+    const [currentReview, setCurrentReview] = useState(review.review)
+    const [currentRating, setCurrentRating] = useState(review.rating)
 
     const user = useContext(UserContext)
 
-    if (user.id == review.user_id) {
-        return <h3>Your review!</h3>
-    } else {
-        return <h3>Another user's review</h3>
-    }
-    
+    const formik = useFormik({
+        initialValues: {
+            review: review.review,
+            rating: String(review.rating)
+        },
+        onSubmit: (values) => {
+            const update = {
+                review: values.review,
+                rating: parseInt(values.rating)
+            }
+            fetch(`/reviews/${review.id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(update)
+            })
+            .then(response => response.json())
+            .then(reviewData => {
+                setCurrentReview(reviewData.review)
+                setCurrentRating(reviewData.rating)
+            })
+            .then(setEditMode(false))
+        }
+    })
 
+    function toggleEdit() {
+        setEditMode(!editMode)
+    }
+
+    function review_edit() {
+        if (editMode) {
+            return(
+                <form onSubmit={formik.handleSubmit}>
+                    <FormLabel>Review</FormLabel>
+                    <Textarea name="review" value={formik.values.review} onChange={formik.handleChange}/>
+                    <FormLabel>Rating</FormLabel>
+                    <Select name="rating" value={formik.values.rating} onChange={formik.handleChange}>
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                    </Select>
+                    <Button type="submit">Submit</Button>
+                    <Button onClick={toggleEdit}>Cancel</Button>
+                </form>
+            )
+
+        } else {
+            return (
+                <>
+                <CardHeader>
+                    <Heading as="h4" size="sm">You</Heading>
+                </CardHeader>
+                <CardBody>{review.review}</CardBody>
+                <CardFooter>
+                    <Button onClick={toggleEdit}>Edit</Button>
+                    <Button>Delete</Button>
+                </CardFooter>
+                </>
+            )
+        }
+    }
+
+    const user_review = review_edit()
+
+    if (user.id === review.user_id) {
+        return (
+            <Card border='1px'>
+                {user_review}
+            </Card>
+        )
+    } else {
+        return (
+            <Card border='1px'>
+                <CardHeader><Heading as="h4" size="sm">User</Heading></CardHeader>
+                <CardBody>{review.review}</CardBody>
+            </Card>
+        )
+    }
 }
 
 export default ReviewCard;
