945af861527d22283422c4d5a948bf5c562565ea
Bianca Aspin <bianca@mj94.net>
2023-04-11 10:07:12 -0700

    Seed database with reviews for frontend development and testing.

--- server/seed.py
+++ server/seed.py
@@ -1,10 +1,62 @@
 from app import app
-from models import db, User
+from models import db, User, Review
+from faker import Faker
 
 with app.app_context():
-    User.query.delete()
     
-    print("Deleting existing data...")
+    fake = Faker()
+
+    # for i in range(5):
+    #     user = User(
+    #         first_name = fake.first_name(),
+    #         last_name = fake.last_name(),
+    #         email = fake.email()
+    #     )
+    #     user.password_hash = fake.word()
+    #     db.session.add(user)
+    #     db.session.commit()
+
+    review_1 = Review(
+        park_id = 465,
+        user_id = 2, 
+        review = fake.paragraph(),
+        rating = 5
+    )
+    db.session.add(review_1)
+
+    review_2 = Review(
+        park_id = 239,
+        user_id = 3, 
+        review = fake.paragraph(),
+        rating = 4
+    )
+
+    db.session.add(review_2)
+
+    review_3 = Review(
+        park_id = 262,
+        user_id = 4, 
+        review = fake.paragraph(),
+        rating = 3
+    )
+
+    db.session.add(review_3)
+
+    review_4 = Review(
+        park_id = 367,
+        user_id = 5, 
+        review = fake.paragraph(),
+        rating = 2
+    )
+
+    db.session.add(review_4)
+
+    review_5 = Review(
+        park_id = 188,
+        user_id = 6, 
+        review = fake.paragraph(),
+        rating = 1
+    )
+
+    db.session.add(review_5)
     db.session.commit()
-    
-    print("Table successfully cleared!")
