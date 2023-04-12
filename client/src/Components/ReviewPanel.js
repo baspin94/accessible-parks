0fc0de092a97f8078a3466b98fe7a03903ce43e8
Bianca Aspin <bianca@mj94.net>
2023-04-11 20:05:45 -0700

    Users can now submit reviews on a park's page. Reviews don't render on DOM until after refresh.

--- client/src/Components/ReviewPanel.js
+++ client/src/Components/ReviewPanel.js
@@ -2,8 +2,19 @@ import {
     Box, 
     Heading,
     UnorderedList,
+    FormControl,
+    FormLabel,
+    Textarea,
+    Select,
+    Button,
+    Stack
 } from '@chakra-ui/react';
 import ReviewCard from './ReviewCard';
+import { UserContext } from '../App';
+import { useFormik } from 'formik';
+import { useContext, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import * as yup from 'yup';
 
 function ReviewPanel({ reviews, setReviews }) {
 
@@ -11,13 +22,81 @@ function ReviewPanel({ reviews, setReviews }) {
         return <ReviewCard key={element.id} review={element} reviewArray={reviews} setReviews={setReviews}/>
     })
 
+    const user = useContext(UserContext)
+    const params = useParams();
+    const parkId = params['id'];
+
+    const formSchema = yup.object().shape({
+        review: yup.string().required("Review cannot be empty."),
+        rating: yup.string().required("Must enter rating.")
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            review: "",
+            rating: ""
+        },
+        validationSchema: formSchema,
+        validateOnChange: false,
+        validateOnBlur: false,
+        onSubmit: (values, { resetForm }) => {
+            const submission = {
+                user_id: user.id,
+                park_id: parseInt(parkId),
+                review: values.review,
+                rating: parseInt(values.rating)
+            }
+            fetch('/reviews', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(submission)
+            })
+                .then(response => response.json())
+                .then(reviewData => {
+                    const updatedReviews = [...reviews, reviewData]
+                    setReviews(updatedReviews)
+                    resetForm({values: ""})
+                })
+        }
+    })
+
+    function review_create() {
+        if (user.id !== undefined) {
+            return(
+                <form onSubmit={formik.handleSubmit}>
+                    <FormLabel>Your Review</FormLabel>
+                    <Textarea name="review" value={formik.values.review} onChange={formik.handleChange}/>
+                    <FormLabel>Rating</FormLabel>
+                    <Select name="rating" placeholder="--" value={formik.values.rating} onChange={formik.handleChange}>
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                    </Select>
+                    <Button type="submit">Submit</Button>
+                </form>
+            )
+        } else {
+            return <p>You must be logged in to leave a review.</p>
+        }
+    }
+
+    const review_form = review_create()
+
+
     return (
-        <Box p="10px">
+        <Stack p='15px'>
             <Heading as="h3" size="lg">Reviews</Heading>
+            <Box border='1px' p='10px'>
+                {review_form}
+            </Box>
             <UnorderedList>
                 {review_cards}
             </UnorderedList>
-        </Box>
+        </Stack>
     )
 }
 
