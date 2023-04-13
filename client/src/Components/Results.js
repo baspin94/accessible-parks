4763da8eadbe392e1ac8a0ecca2f7b2ba8516365
Bianca Aspin <bianca@mj94.net>
2023-04-13 11:21:26 -0700

    Users can now filter results by state. They can also switch which state they're filtering by without refreshing the page.

--- client/src/Components/Results.js
+++ client/src/Components/Results.js
@@ -4,23 +4,74 @@ import {
     Grid,
     Heading,
     Box,
-    Text
-} from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+    Text,
+    FormLabel,
+    Select
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useFormik } from 'formik';
 
 function Results({ parks }) {
+    const [displayParks, setDisplayParks] = useState(parks)
+    const [states, setStates] = useState([])
 
-    const cards = parks.map(park => {
+    useEffect(() => {
+        function getStates() {
+            let statesArray = []
+            parks.forEach(park => {
+                if (park.states.length === 2) {
+                    statesArray.push(park.states)
+                } else {
+                    const states = park.states.split(',')
+                    states.forEach(state => {
+                        statesArray.push(state)
+                    })
+                }
+            })
+            const uniqueStates = [...new Set(statesArray)]
+            return uniqueStates.sort()
+        
+        }
+        const states = getStates()
+        setStates(states)
+    }, [parks])
+
+    const formik = useFormik({
+        initialValues: {
+            state: ""
+        },
+        onSubmit: (values) => {
+            const filterState = values['state']
+            const filteredParks = parks.filter(park => {
+                if (park.states.includes(filterState)){
+                    return park
+                }
+            })
+            setDisplayParks(filteredParks)
+        }
+    })
+
+    const cards = displayParks.map(park => {
         return <ParkCard key={park.id} id={park.id} park={park} />
+    })
+
+    const stateOptions = states.map(state => {
+        const index = states.indexOf(state)
+        return <option key={index} value={state}>{state}</option>
     })
 
     return (
         <Box p="5px" margin="auto" w="80%" textAlign="center">
                 <Heading margin="auto">Search Results</Heading>
-                <Text><strong>{parks.length}</strong> parks matching your search criteria</Text>
-                <Link exact to='/'>
-                    <Button>Search Again</Button>
-                </Link>
+                <Text><strong>{displayParks.length}</strong> parks matching your search criteria</Text>
+                <form onSubmit={formik.handleSubmit}>
+                    <FormLabel>Filter By State:</FormLabel>
+                    <Select name="state" placeholder="Select a state:" value={formik.values.state} onChange={formik.handleChange}>
+                        {stateOptions}
+                    </Select>
+                    <Button type="submit">Filter</Button>
+                </form>
                 <Grid p="20px" margin="auto" w="100%" templateColumns='repeat(3, 1fr)' gap={2}>
                     {cards}
                 </Grid>
