b48f37576a376b75aa7ae0e8a995e77dea627a3b
Bianca Aspin <bianca@mj94.net>
2023-04-17 11:06:46 -0700

    Install library for converting state abbreviations to full names, update how states are rendered on Park Detail page.

--- client/src/Components/OverviewPanel.js
+++ client/src/Components/OverviewPanel.js
@@ -2,22 +2,56 @@ import {
     Heading, 
     Text, 
     Image,
-    VStack
+    VStack,
+    UnorderedList,
+    ListItem,
+    Box
 } from '@chakra-ui/react';
 
 function OverviewPanel( { park }) {
 
+    const states = require('us-state-converter')
+
     const designation = (park.designation === "") ? "Destination" : park.designation
+
+    function getFullStates() {
+        if (park.states.length === 2) {
+            return <Text fontSize='lg'>{states.fullName(park.states)}</Text>
+        } else {
+            const stateAbbrs = park.states.split(',')
+            const stateFullNames = stateAbbrs.map(abbr => {
+                if (stateAbbrs.indexOf(abbr) !== 0) {
+                    return <ListItem fontSize='lg' display='inline' p='1'>· {states.fullName(abbr)}</ListItem>
+
+                } else {
+                    return <ListItem fontSize='lg' display='inline' p='1'>{states.fullName(abbr)}</ListItem>
+                }
+                
+            })
+            return stateFullNames
+        }
+    }
+
+    const stateText = getFullStates()
+
+
     return (
         <>
-        <VStack p="10px">
+        <Box margin="auto" p="5px">
+        <VStack p="10px" spacing={5}>
             <Heading>{park.name}</Heading>
-            <Text>{designation} | {park.states}</Text>
-            {/* <Box boxSize="md" border='1px'> */}
-                <Image w="40%" h="40%" src={park.image_url} alt={park.image_alt}/>
-            {/* </Box> */}
+            <Text fontSize='xl' as='i'>{designation}</Text>
+            {park.states.length === 2 
+            ? stateText
+            :
+                <UnorderedList styleType="None" textAlign='center'>
+                {stateText}
+                </UnorderedList>
+            }
+            <Image w="40%" h="40%" src={park.image_url} alt={park.image_alt}/>
             <Text>{park.description}</Text>
         </VStack>
+        </Box> 
         </>
     )
     
