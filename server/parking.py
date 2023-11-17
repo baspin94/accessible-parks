33179e3740ca07ddb7b863f33347fcdff6a96490
Bianca Aspin <bianca@mj94.net>
2023-11-16 21:11:33 -0800

    Write script to query NPS API for parking information and save accessible lots to db.

--- server/parking.py
+++ server/parking.py
@@ -1,14 +1,18 @@
 from app import app
 from dotenv import load_dotenv
 import os, urllib.request, json
+from models import db, ParkingLot 
 
 with app.app_context():
 
     load_dotenv()
 
+    print("Deleting existing entries...")
+    ParkingLot.query.delete()
+
     api_key = os.environ.get('NPS_API_KEY')
 
-    acc_endpoint = f"https://developer.nps.gov/api/v1/parkinglots?api_key={api_key}"
+    acc_endpoint = f"https://developer.nps.gov/api/v1/parkinglots?limit=550&api_key={api_key}"
 
     print("Requesting data on parking...")
 
@@ -22,6 +26,25 @@ with app.app_context():
 
     for lot in acc_parking_data['data']:
         if lot['accessibility']['isLotAccessibleToDisabled'] == True:
-            print(f"{lot['relatedParks'][0]['parkCode']}: {lot['name']} has {lot['accessibility']['numberofAdaSpaces']} accessible spaces.")
+            # print(f"{lot['relatedParks'][0]['parkCode']}: {lot['name']} has {lot['accessibility']['numberofAdaSpaces']} accessible spaces.")
+        
+            new_lot = ParkingLot(
+                park_code = lot['relatedParks'][0]['parkCode'],
+                name = lot['name'],
+                gen_description = lot['description'],
+                latitude = lot['latitude'],
+                longitude = lot['longitude'],
+                total_spaces = lot['accessibility']['totalSpaces'],
+                total_ada_spaces = lot['accessibility']['numberofAdaSpaces'],
+                van_accessible_spaces = lot['accessibility']['numberofAdaVanAccessbileSpaces'],
+                step_free_spaces = lot['accessibility']['numberofAdaStepFreeSpaces'],
+                oversize_vehicle_spaces = lot['accessibility']['numberOfOversizeVehicleSpaces'],
+                ada_description = lot['accessibility']['adaFacilitiesDescription']
+            )
 
-    print("Done!")
+            db.session.add(new_lot)
+
+    print("Committing parking lots to database...")
+    db.session.commit()
+
+    print("Parking Lots Seeded!")
